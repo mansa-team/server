5f102135f0eb36d3302bdb5b1863b66d31c706be
heitorrosa <76885858+heitorrosa@users.noreply.github.com>
2026-08-04 18:30:37 -0300

    chain workspacePath migration onto head

--- migrations/versions/e5f6a7b8c9d0_drop_workspace_path_prometheus_sandboxes.py
+++ migrations/versions/e5f6a7b8c9d0_drop_workspace_path_prometheus_sandboxes.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "e5f6a7b8c9d0"
-down_revision: Union[str, None] = "d3e4f5a6b7c8"
+down_revision: Union[str, None] = "d3df3fc5ca72"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
